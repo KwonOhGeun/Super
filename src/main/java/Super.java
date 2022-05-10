ffb10080e99ad579b06f41aca21471a130622760
KwonOhGeun <ohgeun72@naver.com>
2022-05-10 16:10:17 +0900

    Initial commit

---
+++ src/main/java/Super.java
@@ -0,0 +1,23 @@
+import javafx.scene.Parent;
+
+public class Super {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.method();
+    }
+}
+
+class parent {
+    int x = 10;
+}
+
+class Child extends parent {
+    int x = 20;
+
+    void method() {
+        System.out.println("x=" + x);
+        System.out.println("this.x=" + this.x);
+        System.out.println("super.x=" + super.x);
+    }
+
+}
